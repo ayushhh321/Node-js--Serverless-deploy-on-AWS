582fd52836128c7f2292e830ef05e3490317f0cf
ayushhh321 <ayush56039@gmail.com>
2024-06-09 17:49:53 +0530

    env added to config

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA5DJ7I5E2CJMK4B4T"
-  secret_key = "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"
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
 }
